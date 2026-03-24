8fe22ba1fd8c3c1d3e06a8ca2b84b6e003512b77
imnotrenn <delacruzrence45@gmail.com>
2026-03-25 07:33:51 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1956343784"
+    serviceWorkerVersion: "2465989982"
   }
 });
